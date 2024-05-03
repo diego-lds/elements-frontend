39a1808e9b8696f9990847792462d119b7bd0cbe
Diego Lopes <diegolopes087@gmail.com>
2024-05-03 00:00:25 -0300

    Revert "refactoring"
    This reverts commit 18be4a2d31d2b5b75e81690c4b34d0d3cc5178d6.

--- src/App.jsx
+++ src/App.jsx
@@ -5,72 +5,85 @@ import Filter from "./components/filter/Filter";
 import Quiz from "./components/quiz/Quiz";
 import Header from "./components/header/Header";
 import Container from "./components/container/Container";
-import useInfiniteScroll from "./hooks/useInfiniteScroll";
-import { buildUrlWithFilters } from "./utils/filtersUtils";
 import "./App.css";
+import useInfiniteScroll from "./hooks/useInfiniteScroll";
 
-const BASE_URL = import.meta.env.VITE_API_URL;
+const API = import.meta.env.VITE_API_URL;
 const INITIAL_PAGE = 1;
 
 function App() {
   const [products, setProducts] = useState([]);
-  const [pagination, setPagination] = useState(INITIAL_PAGE);
+  const [page, setPage] = useState(INITIAL_PAGE);
   const [loading, setLoading] = useState(false);
   const [filters, setFilters] = useState({});
   const [questionsList, setQuestionsList] = useState([]);
 
-  const fetchProducts = useCallback(async () => {
+  const handleFilter = useCallback((params) => {
+    setFilters(params);
+    setPage(INITIAL_PAGE);
+  }, []);
+
+  const fetchData = useCallback(async () => {
     setLoading(true);
     try {
-      const url = buildUrlWithFilters(BASE_URL, pagination, filters);
+      let url = `${API}/products?page=${page}`;
+      const filterParams = new URLSearchParams();
+
+      if (filters && Object.keys(filters).length > 0) {
+        Object.entries(filters).forEach(([key, value]) => {
+          if (value) {
+            filterParams.append(key, value);
+          }
+        });
+        url += `&${filterParams.toString()}`;
+      }
       const response = await axios.get(url);
 
-      pagination === INITIAL_PAGE
-        ? setProducts(response.data)
-        : setProducts((prev) => [...prev, ...response.data]);
-
+      if (page === INITIAL_PAGE) {
+        setProducts(response.data);
+      } else {
+        setProducts((prev) => [...prev, ...response.data]);
+      }
       setLoading(false);
     } catch (error) {
       console.log(error);
       setLoading(false);
     }
-  }, [pagination, filters]);
+  }, [page, filters]);
 
   const fetchQuestions = useCallback(async () => {
     try {
-      const response = await fetch(`${BASE_URL}/quiz`);
+      const response = await fetch(`${API}/quiz`);
       if (!response.ok) {
         throw new Error("Erro ao buscar os dados");
       }
-      const data = await response.json();
-      setQuestionsList(data);
+      const jsonData = await response.json();
+      setQuestionsList(jsonData);
     } catch (error) {
       console.error("Erro:", error);
     }
   }, []);
 
+  useEffect(() => {
+    if (page !== INITIAL_PAGE) {
+      fetchData();
+      fetchQuestions();
+    }
+  }, [page, fetchData, fetchQuestions]);
+
+  useEffect(() => {
+    fetchData();
+    fetchQuestions();
+  }, [fetchData, fetchQuestions]);
+
   const keepLoadingData = useCallback(() => {
-    setPagination((prevPage) => prevPage + 1);
+    setPage((prevPage) => prevPage + 1);
   }, []);
 
   useInfiniteScroll(keepLoadingData);
 
-  const handleFilter = useCallback((params) => {
-    setFilters(params);
-    setPagination(INITIAL_PAGE);
-  }, []);
-
-  useEffect(() => {
-    pagination !== INITIAL_PAGE ? fetchProducts() : fetchQuestions();
-  }, [pagination, fetchProducts, fetchQuestions]);
-
-  useEffect(() => {
-    fetchProducts();
-    fetchQuestions();
-  }, [fetchProducts, fetchQuestions]);
-
   return (
-    <main className="app">
+    <div className="app">
       <Header />
       <Container>
         <h2>Listagem de produtos</h2>
@@ -78,7 +91,7 @@ function App() {
         <Gallery products={products} />
         <Quiz questions={questionsList} />
       </Container>
-    </main>
+    </div>
   );
 }
 
